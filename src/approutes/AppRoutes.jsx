4c97953e313e3d2ef69a9e6a16134562addcd525
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-07-29 16:33:34 -0400

    corrections made

--- src/approutes/AppRoutes.jsx
+++ src/approutes/AppRoutes.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import AddPost from "../pages/AddPost";
+import Post from "../pages/Post";
 
 function AppRoutes() {
   return (
+    // <UserProvider>
+    <BrowserRouter>
+    <NavBar>
     <Routes>
-
-    </div>
+      <Route path="/" element={<Home />} />
+      <Route path="/add" element={<AddPost />} />
+      <Route path="/post" element={<Post />} />
+    </Routes>
+    </NavBar>
+    </BrowserRouter>
+    /* </UserProvider> */
   )
 }
 
-export default AppRoutes
+export default AppRoutes;

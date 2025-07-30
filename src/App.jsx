eebbc36bb562d9893d25598cf442574cdda90984
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-07-30 14:37:20 -0400

    pages created and added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import './App.css'
 import { Routes, Route } from "react-router-dom";
 import AppRoutes from "./approutes/AppRoutes";
 
 function App() {
-
   return (
-    <AuthProvider>
-      <AppRoutes />
-    </AuthProvider>
-  )
+    // <AuthProvider>
+    //   <AppRoutes />
+    // </AuthProvider>
+
+    <h1>Hello, Madeleine</h1>
+  );
 }
 
 export default App;
